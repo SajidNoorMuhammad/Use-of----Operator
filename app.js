865c8d8fb438cd80f5e0db3673a73b06bb9a6c97
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-09 13:06:47 +0500

    Use of ||

---
+++ app.js
@@ -0,0 +1,7 @@
+var course = prompt ("Enter Your Course");
+ 
+if (course === "web" || course === "mobile") {
+    document.write ("You're Enrolled");
+}else {
+    document.write ("Sorry! You're not Enrolled");
+}
