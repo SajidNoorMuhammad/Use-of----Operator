239501541cdf914b667b6d5e6075b119f194290b
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-09 13:21:15 +0500

    Add work

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var course = prompt ("Enter Your Course");
- 
+var course = prompt("Enter Your Course");
+
 if (course === "web" || course === "mobile") {
-    document.write ("You're Enrolled");
-}else {
-    document.write ("Sorry! You're not Enrolled");
+    document.write("You're Enrolled");
+} else {
+    document.write("Sorry! You're not Enrolled");
 }
